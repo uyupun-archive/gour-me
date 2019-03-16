620afde44e2531baa3a322e6199c24c0d8866450
Kazuki Tobita <kazukiti201@gmail.com>
2019-03-16 16:28:02 +0900

    Add axios

--- nuxtjs/pages/index.vue
+++ nuxtjs/pages/index.vue
@@ -5,7 +5,20 @@
 </template>
 
 <script>
-export default {}
+import axios from 'axios';
+
+export default {
+  mounted() {
+    this.fetchRecommendData();
+  },
+  methods: {
+    // 座標データを渡してレコメンドを受け取る
+    async fetchRecommendData() {
+      const { data } = await axios.get('/api/recommend');
+      console.log(data);
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
