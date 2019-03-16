ed99d603b417b859c00ede0272899c2c4545e337
Kazuki Tobita <kazukiti201@gmail.com>
2019-03-16 17:45:39 +0900

    Add PrefectureSeeder

--- database/seeds/PrefectureSeeder.php
+++ database/seeds/PrefectureSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Prefecture;
+use GuzzleHttp\Client as Guzzle;
 
 class PrefectureSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
-     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function run()
     {
-        //
+        $prefectures = json_decode((new Guzzle())->request('GET', 'https://opendata.resas-portal.go.jp/api/v1/prefectures', [
+            'headers' => [
+                'Accept' => 'application/json',
+                'X-API-KEY' => env('X_API_KEY'),
+            ]
+        ])->getBody())->result;
+
+        foreach ($prefectures as $prefecture) {
+            Prefecture::create([
+                'name' => $prefecture->prefName,
+            ]);
+        }
     }
 }
