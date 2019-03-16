f9e76be7c1a8612a857b6e8588efa31a5bd8d7d4
Kazuki Tobita <kazukiti201@gmail.com>
2019-03-17 01:19:52 +0900

    Modify API

--- app/Http/Controllers/RecommendController.php
+++ app/Http/Controllers/RecommendController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recommend;
 use Illuminate\Http\Request;
 
 class RecommendController extends Controller
@@ -9,7 +10,7 @@ class RecommendController extends Controller
     /**
      * レコメンドを行い, その結果を返すAPI
      *
-     * リクエスト: 座標のJSONデータ { x: XX, y: YY }
+     * リクエスト: 座標のJSONデータ { kotteriLevel: XX, gatturiLevel: YY }
      * レスポンス: リクエストの座標に最も近似する座標に対応する食べ物のJSONデータ
      *
      * @return array
@@ -18,7 +19,7 @@ class RecommendController extends Controller
     {
         $request = request();
 
-        // TODO: API作成
-        return [ 'status' => 'OK' ];
+        $recommend = Recommend::first();
+        return $recommend;
     }
 }
