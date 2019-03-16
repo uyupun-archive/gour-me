4e5cf541dc699b8329130d5c9160d79e8219d72c
Kazuki Tobita <kazukiti201@gmail.com>
2019-03-17 02:15:22 +0900

    Modify Recommend API

--- app/Http/Controllers/RecommendController.php
+++ app/Http/Controllers/RecommendController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Recommend;
 use Illuminate\Http\Request;
+use Validator;
 
 class RecommendController extends Controller
 {
@@ -13,13 +14,39 @@ class RecommendController extends Controller
      * リクエスト: 座標のJSONデータ { kotteriLevel: XX, gatturiLevel: YY }
      * レスポンス: リクエストの座標に最も近似する座標に対応する食べ物のJSONデータ
      *
-     * @return array
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     public function index()
     {
         $request = request();
 
-        $recommend = Recommend::first();
-        return $recommend;
+        // バリデーション
+        $validator = Validator::make($request->all(), [
+            'kotteriLevel' => [ 'bail', 'required', 'integer', 'min:0', 'max:100', ],
+            'gatturiLevel' => [ 'bail', 'required', 'integer', 'min:0', 'max:100', ],
+        ]);
+        if ($validator->fails()) return response([], 400);
+
+        $recommends = Recommend::all();
+        $response = new \stdClass();
+
+        // リクエストのこってり度とがっつり度とDBのこってり度とがっつり度の差が最小のものを求める(近似値)
+        $minDiffKotteriLevel = 100;
+        $minDiffGatturiLevel = 100;
+        foreach ($recommends as $recommend) {
+            $absKotteriLevel = abs($request->kotteriLevel - $recommend->kotteri_level);
+            $absGatturiLevel = abs($request->gatturiLevel - $recommend->gatturi_level);
+
+            // こってり度/がっつり度の両方を書き換えるケース
+            if ($minDiffKotteriLevel > $absKotteriLevel &&
+                $minDiffGatturiLevel > $absGatturiLevel) {
+                $minDiffKotteriLevel = $absKotteriLevel;
+                $minDiffGatturiLevel = $absGatturiLevel;
+
+                $response = $recommend;
+            }
+        }
+
+        return response($response, 200);
     }
 }
